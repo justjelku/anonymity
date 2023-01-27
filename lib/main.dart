8b28582eea02d8cd52ad0e07de73777a2ee7966a
Jeremiah Larry King Ungsod <67980301+Jeremiah019@users.noreply.github.com>
2023-01-28 02:11:58 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp( MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Anonymity',
+    theme: ThemeData.dark(),
+    )
+  );
+}
+
